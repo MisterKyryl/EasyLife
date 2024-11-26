a774e3e4660160da5402505dcad9c1a84d71897d
Kyryl <99047616+MisterKyryl@users.noreply.github.com>
2024-11-26 21:49:56 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 (() => {
     "use strict";
+    const modules_flsModules = {};
+    function addLoadedClass() {
+        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
+            setTimeout((function() {
+                document.documentElement.classList.add("loaded");
+            }), 0);
+        }));
+    }
     let bodyLockStatus = true;
     let bodyLockToggle = (delay = 500) => {
         if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
@@ -41,6 +49,16 @@
                 bodyLockToggle();
                 document.documentElement.classList.toggle("menu-open");
             }
+        }));
+    }
+    function functions_FLS(message) {
+        setTimeout((() => {
+            if (window.FLS) console.log(message);
+        }), 0);
+    }
+    function uniqArray(array) {
+        return array.filter((function(item, index, self) {
+            return self.indexOf(item) === index;
         }));
     }
     function ssr_window_esm_isObject(obj) {
@@ -3773,6 +3791,7 @@
             pagination: {
                 el: ".portfolio__pagination",
                 clickable: true,
+                dynamicBullets: true,
                 renderBullet: function(index, className) {
                     const formattedIndex = index < 9 ? `0${index + 1}` : `${index + 1}`;
                     return `<span class="${className}">${formattedIndex}</span>`;
@@ -3794,6 +3813,39 @@
                 },
                 1100: {
                     slidesPerView: 4
+                }
+            },
+            on: {}
+        });
+        if (document.querySelector("result__slider")) new swiper_core_Swiper(".result__slider", {
+            modules: [ Navigation, Pagination ],
+            observer: true,
+            observeParents: true,
+            slidesPerView: 1,
+            spaceBetween: 24,
+            autoHeight: true,
+            speed: 800,
+            pagination: {
+                el: ".result__pagination",
+                clickable: true,
+                dynamicBullets: true
+            },
+            navigation: {
+                prevEl: ".result__button-prev",
+                nextEl: ".result__button-next"
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1
+                },
+                480: {
+                    slidesPerView: 1
+                },
+                768: {
+                    slidesPerView: 1
+                },
+                1100: {
+                    slidesPerView: 1
                 }
             },
             on: {}
@@ -3845,6 +3897,109 @@
     window.addEventListener("load", (function(e) {
         initSliders();
     }));
+    class ScrollWatcher {
+        constructor(props) {
+            let defaultConfig = {
+                logging: true
+            };
+            this.config = Object.assign(defaultConfig, props);
+            this.observer;
+            !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
+        }
+        scrollWatcherUpdate() {
+            this.scrollWatcherRun();
+        }
+        scrollWatcherRun() {
+            document.documentElement.classList.add("watcher");
+            this.scrollWatcherConstructor(document.querySelectorAll("[data-watch]"));
+        }
+        scrollWatcherConstructor(items) {
+            if (items.length) {
+                this.scrollWatcherLogging(`Прокинувся, стежу за об'єктами (${items.length})...`);
+                let uniqParams = uniqArray(Array.from(items).map((function(item) {
+                    if (item.dataset.watch === "navigator" && !item.dataset.watchThreshold) {
+                        let valueOfThreshold;
+                        if (item.clientHeight > 2) {
+                            valueOfThreshold = window.innerHeight / 2 / (item.clientHeight - 1);
+                            if (valueOfThreshold > 1) valueOfThreshold = 1;
+                        } else valueOfThreshold = 1;
+                        item.setAttribute("data-watch-threshold", valueOfThreshold.toFixed(2));
+                    }
+                    return `${item.dataset.watchRoot ? item.dataset.watchRoot : null}|${item.dataset.watchMargin ? item.dataset.watchMargin : "0px"}|${item.dataset.watchThreshold ? item.dataset.watchThreshold : 0}`;
+                })));
+                uniqParams.forEach((uniqParam => {
+                    let uniqParamArray = uniqParam.split("|");
+                    let paramsWatch = {
+                        root: uniqParamArray[0],
+                        margin: uniqParamArray[1],
+                        threshold: uniqParamArray[2]
+                    };
+                    let groupItems = Array.from(items).filter((function(item) {
+                        let watchRoot = item.dataset.watchRoot ? item.dataset.watchRoot : null;
+                        let watchMargin = item.dataset.watchMargin ? item.dataset.watchMargin : "0px";
+                        let watchThreshold = item.dataset.watchThreshold ? item.dataset.watchThreshold : 0;
+                        if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) return item;
+                    }));
+                    let configWatcher = this.getScrollWatcherConfig(paramsWatch);
+                    this.scrollWatcherInit(groupItems, configWatcher);
+                }));
+            } else this.scrollWatcherLogging("Сплю, немає об'єктів для стеження. ZzzZZzz");
+        }
+        getScrollWatcherConfig(paramsWatch) {
+            let configWatcher = {};
+            if (document.querySelector(paramsWatch.root)) configWatcher.root = document.querySelector(paramsWatch.root); else if (paramsWatch.root !== "null") this.scrollWatcherLogging(`Эмм... батьківського об'єкта ${paramsWatch.root} немає на сторінці`);
+            configWatcher.rootMargin = paramsWatch.margin;
+            if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
+                this.scrollWatcherLogging(`йой, налаштування data-watch-margin потрібно задавати в PX або %`);
+                return;
+            }
+            if (paramsWatch.threshold === "prx") {
+                paramsWatch.threshold = [];
+                for (let i = 0; i <= 1; i += .005) paramsWatch.threshold.push(i);
+            } else paramsWatch.threshold = paramsWatch.threshold.split(",");
+            configWatcher.threshold = paramsWatch.threshold;
+            return configWatcher;
+        }
+        scrollWatcherCreate(configWatcher) {
+            console.log(configWatcher);
+            this.observer = new IntersectionObserver(((entries, observer) => {
+                entries.forEach((entry => {
+                    this.scrollWatcherCallback(entry, observer);
+                }));
+            }), configWatcher);
+        }
+        scrollWatcherInit(items, configWatcher) {
+            this.scrollWatcherCreate(configWatcher);
+            items.forEach((item => this.observer.observe(item)));
+        }
+        scrollWatcherIntersecting(entry, targetElement) {
+            if (entry.isIntersecting) {
+                !targetElement.classList.contains("_watcher-view") ? targetElement.classList.add("_watcher-view") : null;
+                this.scrollWatcherLogging(`Я бачу ${targetElement.classList}, додав клас _watcher-view`);
+            } else {
+                targetElement.classList.contains("_watcher-view") ? targetElement.classList.remove("_watcher-view") : null;
+                this.scrollWatcherLogging(`Я не бачу ${targetElement.classList}, прибрав клас _watcher-view`);
+            }
+        }
+        scrollWatcherOff(targetElement, observer) {
+            observer.unobserve(targetElement);
+            this.scrollWatcherLogging(`Я перестав стежити за ${targetElement.classList}`);
+        }
+        scrollWatcherLogging(message) {
+            this.config.logging ? functions_FLS(`[Спостерігач]: ${message}`) : null;
+        }
+        scrollWatcherCallback(entry, observer) {
+            const targetElement = entry.target;
+            this.scrollWatcherIntersecting(entry, targetElement);
+            targetElement.hasAttribute("data-watch-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
+            document.dispatchEvent(new CustomEvent("watcherCallback", {
+                detail: {
+                    entry
+                }
+            }));
+        }
+    }
+    modules_flsModules.watcher = new ScrollWatcher({});
     let addWindowScrollEvent = false;
     setTimeout((() => {
         if (addWindowScrollEvent) {
@@ -3939,6 +4094,66 @@
     const da = new DynamicAdapt("max");
     da.init();
     document.addEventListener("DOMContentLoaded", (function() {
+        let lastScrollTop = 0;
+        const header = document.querySelector("header");
+        window.addEventListener("scroll", (function() {
+            const currentScroll = window.scrollY || document.documentElement.scrollTop;
+            if (currentScroll > lastScrollTop) header.classList.add("header--hidden"); else header.classList.remove("header--hidden");
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+        }));
+    }));
+    document.addEventListener("DOMContentLoaded", (function() {
+        const totalFrames = 64;
+        const frameRate = 40;
+        const imageElements = document.querySelectorAll(".image-main");
+        imageElements.forEach((imageElement => {
+            let currentFrame = 1;
+            let direction = 1;
+            let interval;
+            function playFrames() {
+                currentFrame = 1;
+                interval = setInterval((() => {
+                    imageElement.src = `img/cube/cube_blue/${currentFrame}.webp`;
+                    if (currentFrame === totalFrames) direction = -1; else if (currentFrame === 1) direction = 1;
+                    currentFrame += direction;
+                }), frameRate);
+            }
+            function stopFrames() {
+                clearInterval(interval);
+                interval = null;
+            }
+            const observer = new IntersectionObserver((entries => {
+                entries.forEach((entry => {
+                    if (entry.isIntersecting) {
+                        if (!interval) playFrames();
+                    } else stopFrames();
+                }));
+            }), {
+                threshold: .1
+            });
+            observer.observe(imageElement);
+            if (imageElement.getBoundingClientRect().top < window.innerHeight) playFrames();
+        }));
+    }));
+    document.addEventListener("DOMContentLoaded", (function() {
+        const totalImages = 64;
+        let currentImageIndex = 1;
+        const imageElementForScroll = document.querySelector(".services__picture");
+        const servicesContent = document.querySelector(".services__content");
+        if (imageElementForScroll) {
+            function changeImageBasedOnScroll() {
+                const rect = servicesContent.getBoundingClientRect();
+                const contentHeight = servicesContent.offsetHeight;
+                const scrollPercentage = Math.min(Math.max(0, -rect.top / contentHeight), 1);
+                currentImageIndex = Math.floor(scrollPercentage * (totalImages - 1)) + 1;
+                const newImageSrc = `img/cube/cube_blue/${currentImageIndex}.webp`;
+                imageElementForScroll.setAttribute("src", newImageSrc);
+            }
+            window.addEventListener("scroll", changeImageBasedOnScroll);
+            changeImageBasedOnScroll();
+        }
+    }));
+    document.addEventListener("DOMContentLoaded", (function() {
         const form = document.getElementById("contactForm");
         const sentForm = document.getElementById("sentForm");
         const nameInput = document.getElementById("name");
@@ -4027,5 +4242,6 @@
         new CustomSlider(slider);
     }));
     window["FLS"] = true;
+    addLoadedClass();
     menuInit();
 })();
